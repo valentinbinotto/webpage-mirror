d511b13a72c548042510d0dd7a5c80c4f4efd037
Valentin Binotto <valentin@v434project.com>
2024-01-27 16:28:12 +0100

    Create variables.bash

---
+++ variables.bash
@@ -0,0 +1,6 @@
+# Temporärer Ordner in welchem Dateien durch wget abgelegt werden. Wird nach Gebrauch geleert.
+tempdir=/var/wget-temp
+# Logfile in welche Fehler, Hinweise und Informationen des Skripts geschrieben werden.
+logfile=/var/log/wget-mirror.log
+# Ordner, in welchem Webfiles von Apache2 abgelegt werden. Standartmässig /var/www/html
+apachewebdir=/var/www/html
